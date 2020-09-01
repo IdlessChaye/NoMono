67e34e8227f335d42f0917800af64613f54a282b
IdlessChaye <1174932444@qq.com>
2020-09-01 21:23:49 +0800

    A plain commit.

--- Assets/TouhouNingyoMatsuri/Scripts/Src/Utilities/Miscs.cs
+++ Assets/TouhouNingyoMatsuri/Scripts/Src/Utilities/Miscs.cs
@@ -11,19 +11,24 @@ namespace NingyoRi
 
 		#region Manager Getter
 
+		public static ResourceManager GetResourceManager()
+		{
+			return GameManager.Instance.GetManager<ResourceManager>(ManagerType.Resource);
+		}
+
 		public static string TextMapGet(string key)
 		{
 			return GameManager.Instance.GetManager<TextMapManager>(ManagerType.TextMap).Get(key);
 		}
 
+		public static InputManager GetInputManager()
+		{
+			return GameManager.Instance.GetManager<InputManager>(ManagerType.Input);
+		}
+
 		public static UIManager GetUIManager()
 		{
 			return GameManager.Instance.GetManager<UIManager>(ManagerType.UI);
-		}
-
-		public static ResourceManager GetResourceManager()
-		{
-			return GameManager.Instance.GetManager<ResourceManager>(ManagerType.Resource);
 		}
 
 		public static EntityManager GetEntityManager()
